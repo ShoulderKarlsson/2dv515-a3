2a798f97d6f8cb4bdd6d9f4d3f899b17a6b86214
shoulderkarlsson <shoulderkarlsson@gmail.com>
2017-11-28 17:16:28 +0100

    initial

---
+++ src/main/java/a3/searchengine/MainController.java
@@ -0,0 +1,17 @@
+package a3.searchengine;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @RequestMapping("/search")
+    public String search() {
+        
+
+
+
+        return "www.google.com";
+    }
+}
